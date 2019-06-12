056fbaa845b5ec257acc11b7e31c2d2a1ae30b9e
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-06-12 15:23:36 +0100

    Cats and dogs

---
+++ mod-03/src/components/Cat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cat = () => {
+  return (
+    <div>
+      <h2>Cute Cat</h2>
+      <img src="/cat.jpg" alt="Cat" width="400" />
+    </div>
+  );
+};
+
+export default Cat;
---
+++ mod-03/src/components/Dog.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dog = () => {
+  return (
+    <div>
+      <h2>Cute Dog</h2>
+      <img src="/dog.jpg" alt="Dog" width="400" />
+    </div>
+  );
+};
+
+export default Dog;
