8c9ab9a199f8060928be1e65e2ac43a587c337ce
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-06-14 13:15:42 +0100

    Before TS intro

---
+++ mod-09/src/index.js
@@ -0,0 +1,28 @@
+class Calculator {
+  add(x, y) {
+    const result = x + y;
+
+    return result;
+  }
+
+  subtract(x, y) {
+    const result = x - y;
+    return result;
+  }
+}
+
+const x = document.getElementById("x");
+const y = document.getElementById("y");
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const result = document.getElementById("result");
+
+const calculator = new Calculator();
+
+add.addEventListener("click", () => {
+  result.textContent = calculator.add(x.value, y.value);
+});
+
+subtract.addEventListener("click", () => {
+  result.textContent = calculator.subtract(x.value, y.value);
+});
